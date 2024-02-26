f5548566a4408cb9141819d8d12864ba956a9cb8
LAPTOP-VQL2OFUQ\Lubin Reyes <lubinstevenr@gmail.com>
2024-02-25 21:26:03 -0500

    reajuste el tamaño el index

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:    
- #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@127.0.0.1:3306/tiendaProyecto'
+ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@127.0.0.1:3306/tiendaProyecto'
  #SQLALCHEMY_DATABASE_URI = 'mysql+pymsql://root:HHGDd16cE62aB5BfG6CgCDDB64b4CC6g@mysql.railway.internal:40202/biciMotos'
- SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:HHGDd16cE62aB5BfG6CgCDDB64b4CC6g@roundhouse.proxy.rlwy.net:40202/motos2'
+ #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:HHGDd16cE62aB5BfG6CgCDDB64b4CC6g@roundhouse.proxy.rlwy.net:40202/motos2'
  SQLALCHEMY_TRACK_MODIFICATIONS = True
