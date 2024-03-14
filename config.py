df630b1b59b80a5cd6af5de682c864fd6a935a48
LAPTOP-VQL2OFUQ\Lubin Reyes <lubinstevenr@gmail.com>
2024-03-14 10:38:02 -0500

    realize cambios en el archivo config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:    
- SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@127.0.0.1:3306/tiendaProyecto'
- #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:HHGDd16cE62aB5BfG6CgCDDB64b4CC6g@roundhouse.proxy.rlwy.net:40202/motos2'
+ #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@127.0.0.1:3306/tiendaProyecto'
+ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:HHGDd16cE62aB5BfG6CgCDDB64b4CC6g@roundhouse.proxy.rlwy.net:40202/motos2'
  SQLALCHEMY_TRACK_MODIFICATIONS = True
