620e28cefbbcc98230e97247f9c6bbfecaa885fa
Lubin Reyes <practicantesena2@ticsocial.com.co>
2024-10-20 20:00:33 -0500

    [UPDATE] file config.py and __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,39 +5,40 @@ from flask_login import LoginManager
 #from flask_bcrypt import Bcrypt
 import os
 
-# Crear una instancia de LoginManager para manejar la autenticación del usuario
 login_manager = LoginManager()
 db = SQLAlchemy()
-# Definir la función 'create_app' para configurar y crear la aplicación Flask
 def create_app():
-    # Crear una instancia de la aplicación Flask
     app = Flask(__name__)
 
-    # Configurar una clave secreta para la aplicación (importante para la seguridad)
+    # Configurar una clave secreta para la aplicación (importante
+    # para la seguridad)
     app.config['SECRET_KEY'] = os.urandom(24)
-    # Configurar la aplicación utilizando una clase de configuración en 'config.Config'
+    # Configurar la aplicación utilizando una clase de
+    #  configuración en 'config.Config'
     app.config.from_object('config.Config')
-    
+
 
     #bcrypt = Bcrypt(app)
     # Inicializar la instancia de SQLAlchemy ('db') con la aplicación Flask
     db.init_app(app)
 
-    # Inicializar la instancia de LoginManager ('login_manager') con la aplicación Flask
+    # Inicializar la instancia de LoginManager ('login_manager')
+    #  con la aplicación Flask
     login_manager.init_app(app)
     login_manager.login_view = 'autentificacionBp.login'
 
     # Definir una función para cargar usuarios durante la autenticación
     @login_manager.user_loader
     def load_user(user_id):
-       
-        # en este lugar realizo la importacion porque al hacerlo arriba aun no existe el obejto db y no puede inicializar porque aun no existe, asi que entonces lo importo aca abajo para que se ejecute en el mometo en que ya este creado 
+
         from app.models.Clientes import Clientes
          # Utilizar el ID del usuario para buscar y cargar el usuario desde la base de datos
         return Clientes.query.get(int(user_id))
-    
-    # Importar las rutas desde los módelos y registrarlas en la aplicación
-    from app.routes import clienteRoute, productoRoute, autentificacionRoute, facturaVentasRoute,carritoVentasRoute, detalleVentaRoute,administradorRoute
+
+    from app.routes import (
+    clienteRoute, productoRoute, autentificacionRoute, facturaVentasRoute,
+     carritoVentasRoute, detalleVentaRoute,administradorRoute)
+
     app.register_blueprint(clienteRoute.bp)
     app.register_blueprint(productoRoute.bp)
     app.register_blueprint(autentificacionRoute.bp)
@@ -46,5 +47,4 @@ def create_app():
     app.register_blueprint(detalleVentaRoute.bp)
     app.register_blueprint(administradorRoute.bp)
 
-    # Retornar la aplicación configurada
     return app
