1657b64bc57825f7861b3933117cb030cca8c3e6
Lubin Reyes <practicantesena2@ticsocial.com.co>
2024-10-22 11:57:14 -0500

    [ADD] and create files to project deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,38 +1,27 @@
-# Importar la clase Flask y SQLAlchemy desde sus respectivos modelos
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_migrate import Migrate
 #from flask_bcrypt import Bcrypt
 import os
 
 login_manager = LoginManager()
 db = SQLAlchemy()
+migrate = Migrate()
 def create_app():
     app = Flask(__name__)
 
-    # Configurar una clave secreta para la aplicación (importante
-    # para la seguridad)
     app.config['SECRET_KEY'] = os.urandom(24)
-    # Configurar la aplicación utilizando una clase de
-    #  configuración en 'config.Config'
     app.config.from_object('config.Config')
-
-
     #bcrypt = Bcrypt(app)
-    # Inicializar la instancia de SQLAlchemy ('db') con la aplicación Flask
     db.init_app(app)
-
-    # Inicializar la instancia de LoginManager ('login_manager')
-    #  con la aplicación Flask
+    migrate.init_app(app, db)
     login_manager.init_app(app)
     login_manager.login_view = 'autentificacionBp.login'
 
-    # Definir una función para cargar usuarios durante la autenticación
     @login_manager.user_loader
     def load_user(user_id):
-
         from app.models.Clientes import Clientes
-         # Utilizar el ID del usuario para buscar y cargar el usuario desde la base de datos
         return Clientes.query.get(int(user_id))
 
     from app.routes import (
--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
 class Config:
- SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@127.0.0.1:3306/tiendaProyecto'
+ SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
+        f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+    )
  #SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:HHGDd16cE62aB5BfG6CgCDDB64b4CC6g@roundhouse.proxy.rlwy.net:40202/motos2'
- SQLALCHEMY_TRACK_MODIFICATIONS = True
+ SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
