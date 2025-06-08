3a7e5328b8767fd3f10b4766481f93fb42e6f17d
Lubin0123 <lubinstevenr@gmail.com>
2025-06-07 20:58:54 -0500

    ajustes de archivos para despligue en el servidor

--- app/__init__.py
+++ app/__init__.py
@@ -2,28 +2,37 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
-#from flask_bcrypt import Bcrypt
+from config import ProductionConfig, DevelopmentConfig
 import os
+
+#from flask_bcrypt import Bcrypt
 
 login_manager = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
 def create_app():
     app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config.from_object('config.Config')
+    # Detectar entorno desde variable FLASK_ENV
+    Flask_ENV = os.getenv('FLASK_ENV', 'development')
+    
+    if Flask_ENV == 'production':
+        app.config.from_object('config.ProductionConfig')
+        print("🚀 Modo Producción Activo")
+    else:
+        app.config.from_object('config.DevelopmentConfig')
+        print("🛠️ Modo Desarrollo Activo")
     #bcrypt = Bcrypt(app)
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
     login_manager.login_view = 'autentificacionBp.login'
 
+    from app.models import Clientes
     @login_manager.user_loader
     def load_user(user_id):
-        from app.models.Clientes import Clientes
         return Clientes.query.get(int(user_id))
 
+    # Registrar Blueprints
     from app.routes import (
     clienteRoute, productoRoute, autentificacionRoute, facturaVentasRoute,
      carritoVentasRoute, detalleVentaRoute,administradorRoute)
--- run.py
+++ run.py
@@ -8,4 +8,6 @@ with app.app_context():
 
 if __name__ == '__main__':
     # app.run(debug=True, host='127.0.0.1', port=int(os.environ.get('PORT', 8080)))
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    # app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=app.config['DEBUG'])
+
