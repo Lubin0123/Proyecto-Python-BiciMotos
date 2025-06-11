f1dabbe3e644affcdcf5dcdbfad2c96ad7dbb766
Lubin0123 <lubinstevenr@gmail.com>
2025-06-10 19:53:13 -0500

    ajustes en la obtencion del puerto de conexion a la base de datos

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 from dotenv import load_dotenv
 import os
 load_dotenv()
+DB_PORT = os.getenv('DB_PORT', '3306')
 class Config:
     SECRET_KEY= os.getenv("SECRET_KEY", "supersecretkey")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = (
+    SQLALCHEMY_DATABASE_URI = (  
         f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-        f"@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+        f"@{os.getenv('DB_HOST')}:{DB_PORT}/{os.getenv('DB_NAME')}"
     )
 #  SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:HHGDd16cE62aB5BfG6CgCDDB64b4CC6g@roundhouse.proxy.rlwy.net:40202/motos2'
 class ProductionConfig(Config):
